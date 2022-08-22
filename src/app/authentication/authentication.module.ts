6b3b18d9eed178b983ffdb6418ed228627422f1b
Jorge Vergara <jorge@jorgevergara.co>
2022-08-22 15:02:48 -0400

    feat: Added the shell module for authentication

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -7,14 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { AuthenticationPageRoutingModule } from './authentication-routing.module';
 
 import { AuthenticationPage } from './authentication.page';
+import { AuthFormComponent } from './auth-form/auth-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AuthenticationPageRoutingModule
+    AuthenticationPageRoutingModule,
   ],
-  declarations: [AuthenticationPage]
+  declarations: [AuthenticationPage, AuthFormComponent],
 })
 export class AuthenticationPageModule {}
