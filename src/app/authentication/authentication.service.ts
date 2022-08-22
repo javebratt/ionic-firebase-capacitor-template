9f39d1325508a0378a3b6ce151def1f47296cf22
Jorge Vergara <jorge@jorgevergara.co>
2022-08-22 15:10:54 -0400

    feat: Added more authentication functionality to the auth service

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -1,23 +1,61 @@
 import { Injectable } from '@angular/core';
 import {
   Auth,
+  authState,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  sendPasswordResetEmail,
   signInWithEmailAndPassword,
+  signOut,
+  User,
   UserCredential,
 } from '@angular/fire/auth';
+import { doc, Firestore, setDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
-  constructor(private readonly auth: Auth) {}
+  constructor(
+    private readonly auth: Auth,
+    private readonly firestore: Firestore
+  ) {}
 
-  login({
-    email,
-    password,
-  }: {
-    email: string;
-    password: string;
-  }): Promise<UserCredential> {
+  getUser(): Observable<User> {
+    return authState(this.auth);
+  }
+
+  login({ email, password }: AuthCredentials): Promise<UserCredential> {
     return signInWithEmailAndPassword(this.auth, email, password);
   }
+
+  async signup({ email, password }: AuthCredentials): Promise<User> {
+    try {
+      const newUserCredential: UserCredential =
+        await createUserWithEmailAndPassword(this.auth, email, password);
+      const userReference = doc(
+        this.firestore,
+        `users/${newUserCredential.user.uid}`
+      );
+      await setDoc(userReference, { email }, { merge: true });
+      await sendEmailVerification(newUserCredential.user);
+      return newUserCredential.user;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  resetPassword(email: string): Promise<void> {
+    return sendPasswordResetEmail(this.auth, email);
+  }
+
+  logout(): Promise<void> {
+    return signOut(this.auth);
+  }
+}
+
+export interface AuthCredentials {
+  email: string;
+  password: string;
 }
