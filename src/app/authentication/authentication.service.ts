372b7af50b38cf308e1f50cfe15d7bf134362b1d
Jorge Vergara <jorge@jorgevergara.co>
2022-08-22 15:03:03 -0400

    feat: Added login method

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import {
+  Auth,
+  signInWithEmailAndPassword,
+  UserCredential,
+} from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
-  constructor() {}
+  constructor(private readonly auth: Auth) {}
+
+  login({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }): Promise<UserCredential> {
+    return signInWithEmailAndPassword(this.auth, email, password);
+  }
 }
