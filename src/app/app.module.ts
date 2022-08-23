23c67f17a06d3eb6ab642a8751d2f460117bbb28
Wayne Stidolph <wayne@stidolph.com>
2022-08-23 11:33:48 -0700

    for issue initialize Firebase emulator #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,29 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth, getAuth } from '@angular/fire/auth';
+
+import { getApp, initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
 import {
   provideFirestore,
   getFirestore,
   enableIndexedDbPersistence,
+  Firestore,
+  initializeFirestore,
+  connectFirestoreEmulator,
 } from '@angular/fire/firestore';
-import { provideFunctions, getFunctions } from '@angular/fire/functions';
-import { provideStorage, getStorage } from '@angular/fire/storage';
+import {
+  provideFunctions,
+  getFunctions,
+  connectFunctionsEmulator,
+} from '@angular/fire/functions';
+
+import {
+  provideStorage,
+  getStorage,
+  connectStorageEmulator,
+} from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
@@ -24,14 +37,45 @@ import { provideStorage, getStorage } from '@angular/fire/storage';
     IonicModule.forRoot(),
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
     provideFirestore(() => {
-      const firestore = getFirestore();
+      let firestore: Firestore;
+      if (environment.useEmulators) {
+        // Long polling required for Cypress
+        firestore = initializeFirestore(getApp(), {
+          experimentalForceLongPolling: true,
+        });
+        connectFirestoreEmulator(firestore, 'localhost', 8080);
+      } else {
+        firestore = getFirestore();
+      }
       enableIndexedDbPersistence(firestore);
       return firestore;
     }),
-    provideFunctions(() => getFunctions()),
-    provideStorage(() => getStorage()),
+    provideAuth(() => {
+      const auth = getAuth();
+      if (environment.useEmulators) {
+        // note that URL arg to connectAuthEmulator is different style
+        // than emulator URL arg to other connect*Emulator functions
+        connectAuthEmulator(auth, 'http://localhost:9099', {
+          disableWarnings: true,
+        });
+      }
+      return auth;
+    }),
+    provideFunctions(() => {
+      const functions = getFunctions();
+      if (environment.useEmulators) {
+        connectFunctionsEmulator(functions, 'localhost', 5001);
+      }
+      return functions;
+    }),
+    provideStorage(() => {
+      const storage = getStorage();
+      if (environment.useEmulators) {
+        connectStorageEmulator(storage, 'localhost', 9199);
+      }
+      return storage;
+    }),
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
