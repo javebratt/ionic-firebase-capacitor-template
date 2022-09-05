8e0907115f734c1a688453062f1dbb75667e6502
Jorge Vergara <jorge@jorgevergara.co>
2022-09-05 15:33:32 -0400

    chore: Removed unused OnInit import

--- src/app/authentication/login/login.page.ts
+++ src/app/authentication/login/login.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../authentication.service';
