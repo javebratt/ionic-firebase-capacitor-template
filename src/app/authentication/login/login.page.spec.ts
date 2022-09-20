e7d3bce348ac82615f8af0ca18f28769bb831af0
Jorge Vergara <jorge@jorgevergara.co>
2022-09-19 22:37:56 -0400

    test: Added tests for the login page

--- src/app/authentication/login/login.page.spec.ts
+++ src/app/authentication/login/login.page.spec.ts
@@ -1,34 +1,84 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { AlertService } from '../../shared/services/alert.service';
 import { AuthenticationService } from '../authentication.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { LoginPage } from './login.page';
+import { Router } from '@angular/router';
 
+const mockCredentials = { email: 'jorge@jorgevergara.co', password: '123456' };
 const setup = async () => {
   const mockAuthenticationService = jasmine.createSpyObj(
     'AuthenticationService',
     ['login']
   );
+
+  const mockAlertService = jasmine.createSpyObj('AlertService', [
+    'presentInformationAlert',
+  ]);
+
   await TestBed.configureTestingModule({
     declarations: [LoginPage],
-    imports: [IonicModule.forRoot(), ReactiveFormsModule],
+    imports: [IonicModule.forRoot(), ReactiveFormsModule, RouterTestingModule],
     providers: [
       {
         provide: AuthenticationService,
         useValue: mockAuthenticationService,
+      },
+      {
+        provide: AlertService,
+        useValue: mockAlertService,
       },
     ],
   }).compileComponents();
 
   const fixture = TestBed.createComponent(LoginPage);
   const component = fixture.componentInstance;
-  return { fixture, component };
+  const router = TestBed.inject(Router);
+
+  return {
+    fixture,
+    component,
+    mockAuthenticationService,
+    mockAlertService,
+    router,
+  };
 };
 
 describe('LoginPage', () => {
-  it('should create', async () => {
+  it('creates the component', async () => {
     const { component } = await setup();
     expect(component).toBeTruthy();
+  });
+
+  it('calls authentication service login method', async () => {
+    const { component, mockAuthenticationService, router, fixture } =
+      await setup();
+    spyOn(router, 'navigateByUrl');
+    component.login(mockCredentials);
+
+    expect(mockAuthenticationService.login).toHaveBeenCalledTimes(1);
+    expect(mockAuthenticationService.login).toHaveBeenCalledWith(
+      mockCredentials
+    );
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(router.navigateByUrl).toHaveBeenCalledOnceWith('');
+  });
+
+  it('calls alert service when there is an error', async () => {
+    const { component, mockAuthenticationService, fixture, mockAlertService } =
+      await setup();
+
+    mockAuthenticationService.login.and.throwError('error');
+
+    component.login(mockCredentials);
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(mockAlertService.presentInformationAlert).toHaveBeenCalled();
   });
 });
